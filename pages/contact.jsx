a5297db870c13ed686999cfa4a66e969fbc11241
yeifet <yeifettoledo@gmail.com>
2022-04-25 14:03:21 -0500

    Primer commit

---
+++ pages/contact.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+export default function about(){
+    return(
+        <Layout
+            title="Contact | Next.JS"
+            description="Agregue una descripción"
+        >
+            <h1>Contact</h1>
+        </Layout>
+    )
+}
